39d51392c62c10e369149cc5871926ecfab0b3df
EmersonCarlosAlvesSouza <emersoncarlosalves21@gmail.com>
2024-08-31 14:09:24 -0300

    Criado o front inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: "admin", email: "admin@teste.com", phone: "65656565" },
+    { name: "gerente", email: "gerente@teste.com", phone: "78787878" },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2>USUÁRIOS</h2>
+        <form className="user-form">
+          <input type="text" placeholder="Nome" />
+          <input type="email" placeholder="E-mail" />
+          <input type="text" placeholder="Telefone" />
+          <input type="date" placeholder="Data de Nascimento" />
+          <button type="submit">SALVAR</button>
+        </form>
+        <div className="user-list">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Fone</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={index}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>
+                    <button className="edit-btn">✎</button>
+                    <button className="delete-btn">🗑</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
